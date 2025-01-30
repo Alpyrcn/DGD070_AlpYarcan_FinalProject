1ed0159be805004941964561608e0a7ffaa7bdd8
Alpyrcn <119428616+Alpyrcn@users.noreply.github.com>
2025-01-30 18:58:54 +0300

    v2

--- Assets/Scripts/Component/PlayerHealthComponent.cs
+++ Assets/Scripts/Component/PlayerHealthComponent.cs
@@ -6,15 +6,3 @@ public class PlayerHealthComponent : IComponent
 {
     public float Value;
 }
-
-[Game]
-public class PlayerDamagedComponent : IComponent
-{
-    public int damageAmount;
-}
-
-[Game]
-public class PlayerHealedComponent : IComponent
-{
-    public int healAmount;
-}
